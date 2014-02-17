5e9ed8b32f322ae247e93d35ba083ddede12d487
madhuka <madhukaudantha@gmail.com>
2014-02-17 17:37:29 +0530

    addding sample host object

---
+++ hostObjects/org.jaggeryjs.hostobjects.car/src/main/java/org/jaggeryjs/hostobjects/car/CarHostObject.java
@@ -0,0 +1,105 @@
+package org.jaggeryjs.hostobjects.car;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jaggeryjs.scriptengine.exceptions.ScriptException;
+import org.jaggeryjs.scriptengine.util.HostObjectUtil;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Function;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+import org.jaggeryjs.test.car.Engine;
+
+public class CarHostObject  extends ScriptableObject {
+
+	private static final Log log = LogFactory.getLog(CarHostObject.class);
+	private static final String MODULE_NAME = "car";
+	private static CarHostObject car = null;
+	private static Engine engine = null;
+	
+    /**
+     * Constructor for the use by Rhino
+     */
+    public CarHostObject() {
+    }
+
+    /**
+     * Constructor the user will be using inside javaScript
+     */
+    public static Scriptable jsConstructor(Context cx, Object[] args, Function ctorObj,
+                                           boolean inNewExpr) throws ScriptException {
+        car = new CarHostObject();
+        engine = new Engine();
+        return car;
+    }
+    
+
+	public static void jsFunction_start(Context cx, Scriptable thisObj,
+			Object[] args, Function funObj) throws ScriptException {
+		String functionName = "start";
+		int argsCount = args.length;
+		if (argsCount != 0) {
+			HostObjectUtil.invalidNumberOfArgs(MODULE_NAME, functionName,
+					argsCount, true);
+		}
+		
+		engine.start();
+	}
+
+	public static void jsFunction_stop(Context cx, Scriptable thisObj,
+			Object[] args, Function funObj) throws ScriptException {
+		String functionName = "stop";
+		int argsCount = args.length;
+		if (argsCount != 0) {
+			HostObjectUtil.invalidNumberOfArgs(MODULE_NAME, functionName,
+					argsCount, true);
+		}
+		
+		engine.stop();
+	}
+
+	public static String jsGet_carStatus(Context cx, Scriptable thisObj,
+			Object[] args, Function funObj) throws ScriptException {
+		String functionName = "carStatus";
+		int argsCount = args.length;
+		if (argsCount != 0) {
+			HostObjectUtil.invalidNumberOfArgs(MODULE_NAME, functionName,
+					argsCount, true);
+		}
+
+		return engine.getEngineStatus();
+	}
+
+	public static String drive(Context cx, Scriptable thisObj, Object[] args,
+			Function funObj) throws ScriptException {
+		String functionName = "drive";
+		int argsCount = args.length;
+		if (argsCount != 0 && argsCount != 2) {
+			HostObjectUtil.invalidNumberOfArgs(MODULE_NAME, functionName,
+					argsCount, true);
+		}
+		if (argsCount == 2) {
+			if (!(args[0] instanceof String)) {
+				HostObjectUtil.invalidArgsError(MODULE_NAME, functionName, "1",
+						"string", args[1], true);
+			}
+
+			if (!(args[1] instanceof Number)) {
+				HostObjectUtil.invalidArgsError(MODULE_NAME, functionName, "2",
+						"number", args[1], true);
+			}
+			String driverName = args[0].toString();
+			Float speed = Float.parseFloat(args[1].toString());
+			return driverName + " is driving car with speed" + speed;
+		}
+Engine eng  = new Engine();
+eng.start();
+eng.getEngineStatus();
+		return "car is driving";
+	}
+
+	@Override
+	public String getClassName() {
+		return "car";
+	}
+}
---
+++ hostObjects/org.jaggeryjs.test.car/src/main/java/org/jaggeryjs/test/car/Engine.java
@@ -0,0 +1,31 @@
+package org.jaggeryjs.test.car;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Engine {
+
+	private final Log log = LogFactory.getLog(Engine.class);
+private String engineStatus = "non started";
+	public  void start() {
+		log.info("starting Engine");
+		//Engine is starting.
+		engineStatus =  "Engine Started";
+	}
+	
+	public  void stop() {
+		log.info("stopping Engine");
+		//Engine is stopping.
+		engineStatus =  "Engine stopped";
+	}
+	public  String getEngineStatus() {
+		log.info("Checking Engine Status");
+		return engineStatus;
+	}
+	
+	public  String changeGear(int gear) {
+		log.info("changeGear to "+gear);
+		//Changing gears
+		return "Gear Changed to "+gear;
+	}
+}
